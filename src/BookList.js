17ace54403a090c20fdc0bfb2a3495045d085e51
Natalie <sleeplesspony@gmail.com>
2018-08-09 01:54:12 +0300

    fix: correct Add a book link

--- src/BookList.js
+++ src/BookList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import BookShelf from './BookShelf'
+import { Link } from 'react-router-dom'
 
 class BookList extends Component {
 
@@ -26,7 +27,7 @@ class BookList extends Component {
 	            </div>
             </div>
 	            <div className="open-search">
-	              	<a href="#search">Add a book</a>
+	              	<Link className="close-search" to='/search'>Add a book</Link>
 	            </div>
           	</div>
 		)
