7ea2f8e36ad6f57532f9d38de4399621609e42dc
Natalie <sleeplesspony@gmail.com>
2018-08-09 00:27:15 +0300

    feat: add Book component

---
+++ src/Book.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import ShelfChanger from './ShelfChanger'
+
+class Book extends Component {
+
+	render() {
+
+		return (
+			<div className="book">
+				<div className="book-top">
+				<div className="book-cover" style={{ width: 128, height: 170, backgroundImage: 'url(' + this.props.bookData.imageLinks.thumbnail + ')' }}></div>
+				<ShelfChanger />
+				</div>
+				<div className="book-title">{ this.props.bookData.title }</div>
+				<div className="book-authors">{ this.props.bookData.authors.join(', ') }</div>
+			</div>
+		)
+	}
+}
+
+export default Book
